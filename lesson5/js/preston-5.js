36759fceb133a4bf0ac8ff6e3d4df5486f95ab1f
keviner99 <esp21007@byui.edu>
2021-10-16 21:16:45 -0500

    updated

---
+++ lesson5/js/preston-5.js
@@ -0,0 +1,8 @@
+const hamButton = document.querySelector('.ham');
+const mainMenu = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () =>{mainMenu.classList.toggle('responsive')}, false);
+
+window.onresize = () => {if (window.innerWidth > 760) mainMenu.classList.remove('responsive')};
+
+document.getElementById("currentdate").innerHTML = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"long", day:"numeric"}) 
