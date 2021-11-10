46bd8c5388a735fcbe9cde78b916831e01e56f5d
keviner99 <esp21007@byui.edu>
2021-11-09 19:39:04 -0500

    updated

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -6,17 +6,38 @@ fetch(requestURL)
   })
     
   .then(function (jsonObject) {
-      console.table(jsonObject);  // temporary checking for valid response and data parsing
-      for (let i = 0; i < prophets.length; i++) { }
+    const prophets = jsonObject['prophets'];
+
+    
+    for (let i = 0; i < prophets.length; i++) {
+
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let birth = document.createElement('p');
+      let place = document.createElement('p');
+      let image = document.createElement('img');
+        
+      h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+      birth.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+      place.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+      image.setAttribute('src', prophets[i].imageurl);
+
+
+
+
+      card.appendChild(h2);
+      card.appendChild(birth);
+      card.appendChild(place);
+      card.appendChild(image);
+        
+      document.querySelector('div.cards').appendChild(card);
+
+      }
+  
   });
 
-const prophets = jsonObject['prophets'];
+
   
-let card = document.createElement('section');
-let h2 = document.createElement('h2');
 
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
 
-card.appendChild(h2);
 
-document.querySelector('div.cards').appendChild(card);
