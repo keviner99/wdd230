1b54992ccf0caef8c36582fa4153bc93bbf5bc56
keviner99 <esp21007@byui.edu>
2021-11-19 17:55:59 -0500

    updated

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,36 +1,23 @@
-//Api call by city ID.
-//api.openweathermap.org/data/2.5/weather?id={city id}&appid={API key}
-const APPID = 'e08ff644087305907cc3d796c7e963a2'
-const Preston = 5604473
 
-//const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=${APPID}";
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=e08ff644087305907cc3d796c7e963a2'
 
-function getWeatherFor(city_id) {
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    console.log(jsObject);
 
-  const apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=${city_id}&appid=${APPID}'
+    const currentTemp = document.querySelector('#current-temp');
+    const weathericon = document.querySelector('#imagesrc')
+    
+    
+    
+    let imgsrc = 'https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png';
+    let imgalt = jsObject.weather[0].description;
+
+    currentTemp.textContent = jsObject.main.temp;
+    weathericon.setAttribute('src', imgsrc);
+    weathericon.setAttribute('alt', imgalt);
+
+
+  });
   
-  fetch(apiURL)
-    .then((response) => response.json())
-    .then((weatherJSON) => {
-      console.log(weatherJSON);
-        
-      const currentTemp = weatherJSON.main.temp
-      document.getElementById('current-temp').textContent = currentTemp;
-        
-      const imagesrc = 'https://openweathermap.org/img/w/${weatherJSON.weather[0].icon}.png'
-        
-      const desc = weatherJSON.weather[0].description;
-          
-      document.getElementById('imagesrc').textContent = imagesrc;
-            
-      document.getElementById('icon').setAttribute('src', imagesrc);
-            
-      document.getElementById('icon').setAttribute('alt', desc);
-        
-        
-
-
-
-    })
-}
-getWeatherFor(Preston)
